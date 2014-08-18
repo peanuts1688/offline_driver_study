37e85d69d2a8a6cf71e5171566308d4f221d1e57
jchang <jchang@tensorcom.com>
2014-08-18 11:29:11 -0700

    understanding br_input.c

--- br_input.c
+++ br_input.c
@@ -45,7 +45,7 @@ int br_handle_frame_finish(struct sk_buff *skb)
 	struct net_bridge *br;
 	struct net_bridge_fdb_entry *dst;
 	struct net_bridge_mdb_entry *mdst;
-	struct sk_buff *skb2;
+	struct sk_buff *skb2;   // jchang: this packet go to local host (passes up)
 
 	if (!p || p->state == BR_STATE_DISABLED)
 		goto drop;
@@ -54,10 +54,12 @@ int br_handle_frame_finish(struct sk_buff *skb)
 	br = p->br;
 	br_fdb_update(br, p, eth_hdr(skb)->h_source);
 
+  // jchang: check multicast
 	if (is_multicast_ether_addr(dest) &&
 	    br_multicast_rcv(br, p, skb))
 		goto drop;
 
+  // jchang: check learning
 	if (p->state == BR_STATE_LEARNING)
 		goto drop;
 
@@ -66,40 +68,54 @@ int br_handle_frame_finish(struct sk_buff *skb)
 	/* The packet skb2 goes to the local host (NULL to skip). */
 	skb2 = NULL;
 
+  // jchang: check promiscuous
 	if (br->dev->flags & IFF_PROMISC)
-		skb2 = skb;
+		skb2 = skb;   // jchang: passing skb2 up
 
 	dst = NULL;
 
-	if (is_multicast_ether_addr(dest)) {
+	if (is_multicast_ether_addr(dest)) 
+  {
 		mdst = br_mdb_get(br, skb);
-		if (mdst || BR_INPUT_SKB_CB_MROUTERS_ONLY(skb)) {
+		if (mdst || BR_INPUT_SKB_CB_MROUTERS_ONLY(skb)) 
+    {
 			if ((mdst && !hlist_unhashed(&mdst->mglist)) ||
 			    br_multicast_is_router(br))
+      {
 				skb2 = skb;
+      }
 			br_multicast_forward(mdst, skb, skb2);
 			skb = NULL;
 			if (!skb2)
+      {
 				goto out;
-		} else
+      }
+		} 
+    else
+    {
 			skb2 = skb;
-
+    }
 		br->dev->stats.multicast++;
-	} else if ((dst = __br_fdb_get(br, dest)) && dst->is_local) {
+	} 
+  else if ((dst = __br_fdb_get(br, dest)) && dst->is_local)       // jchang: search for specified MAC destination addr inforwarding table
+  {
 		skb2 = skb;
 		/* Do not forward the packet since it's local. */
 		skb = NULL;
 	}
 
-	if (skb) {
-		if (dst)
+	if (skb) 
+  {
+		if (dst)    // jchang: if no destination addr, it's always flooded
 			br_forward(dst->dst, skb, skb2);
-		else
+		else      // jchang: multicast or broadcast, also passed up to host
 			br_flood_forward(br, skb, skb2);
 	}
 
-	if (skb2)
+	if (skb2)   // jchang: if to pass up to host
+  {
 		return br_pass_frame_up(skb2);
+  }
 
 out:
 	return 0;
@@ -119,9 +135,9 @@ static int br_handle_local_finish(struct sk_buff *skb)
 }
 
 /* Does address match the link local multicast address.
- * 01:80:c2:00:00:0X
+ * 01:80:c2:00:00:0X      // jchang: special PDU of the spanning-tree protocol
  */
-static inline int is_link_local(const unsigned char *dest)
+static inline int is_link_local(const unsigned char *dest)      // jchang: passed to br_stp_handle_pdu() in older modules
 {
 	__be16 *a = (__be16 *)dest;
 	static const __be16 *b = (const __be16 *)br_group_address;
@@ -141,14 +157,15 @@ struct sk_buff *br_handle_frame(struct net_bridge_port *p, struct sk_buff *skb)
 	const unsigned char *dest = eth_hdr(skb)->h_dest;
 	int (*rhook)(struct sk_buff *skb);
 
-	if (!is_valid_ether_addr(eth_hdr(skb)->h_source))
+	if (!is_valid_ether_addr(eth_hdr(skb)->h_source))     // jchang: check for valid ether addr, filtering out invalid packets
 		goto drop;
 
 	skb = skb_share_check(skb, GFP_ATOMIC);
 	if (!skb)
 		return NULL;
 
-	if (unlikely(is_link_local(dest))) {
+	if (unlikely(is_link_local(dest)))      // jchang: special PDU of the spanning-tree protocol
+  {
 		/* Pause frames shouldn't be passed up by driver anyway */
 		if (skb->protocol == htons(ETH_P_PAUSE))
 			goto drop;
@@ -167,7 +184,7 @@ struct sk_buff *br_handle_frame(struct net_bridge_port *p, struct sk_buff *skb)
 forward:
 	switch (p->state) 
   {
-    case BR_STATE_FORWARDING:   // uses a forwarding table to seperate traffic between LAN and within LAN
+    case BR_STATE_FORWARDING:   // jchang: uses a forwarding table to seperate traffic between LAN and within LAN
       rhook = rcu_dereference(br_should_route_hook);
       if (rhook != NULL) 
       {
@@ -176,12 +193,12 @@ forward:
         dest = eth_hdr(skb)->h_dest;
       }
       /* fall through */
-    case BR_STATE_LEARNING:     // maintaining forwarding table
+    case BR_STATE_LEARNING:     // jchang: maintaining forwarding table
       if (!compare_ether_addr(p->br->dev->dev_addr, dest))
         skb->pkt_type = PACKET_HOST;
 
       NF_HOOK(NFPROTO_BRIDGE, NF_BR_PRE_ROUTING, skb, skb->dev, NULL,
-          br_handle_frame_finish);
+          br_handle_frame_finish);    // jchang: check for various situation, promiscuous mode etc.
       break;
     default:
 drop:
